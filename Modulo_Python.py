d51cec3ccd8b6fac9f2f40799d2336ea39c7f014
Udem <udem@fedora.mshome.net>
2017-07-03 18:02:09 -0600

    Modulos

---
+++ Modulo_Python.py
@@ -0,0 +1,9 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print 'Sirviendo en el puerto {0}'.format(PORT)
+httpd.serve_forever()
