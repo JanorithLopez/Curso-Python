c08a5b3cc1d224ff476192a237add59705c1a592
Udem <udem@fedora.mshome.net>
2017-07-05 17:52:03 -0600

    Documentos con formulario (vista)

--- python_web/proyecto_web/documentos/views.py
+++ python_web/proyecto_web/documentos/views.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Importar clase base para Vistas
+# Las vistas manejan la logica del sitio web
+from django.views.generic import View
+
+# Metodod render retorna una respuesta
+# Combina una plantilla con un diccionario de contexto
+# Y retorna un objeto HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from documentos.models import Documento
+
+# Create your views here..
+
+# Crear vista de Documentos
+# Definir metodo get
+class Documentos(View):
+	def get(self, request, *args, **kwargs):
+
+		docs = Documento.objects.all()
+
+		context = {
+		    'docs': docs,
+		    'encabezado': 'Mis documentos'
+		}
+
+		return render(
+			request,
+			'documento.html',
+			context
+		)
